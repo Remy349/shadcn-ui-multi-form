99b8dc8e938aaa74c8d1013d5f1968838d59653e
Remy349 <santoraga15@gmail.com>
2025-01-29 15:57:47 -0600

    modify: Minor changes

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export const Footer = () => {
   return (
-    <footer className="pt-[2.5rem] pb-[2.5rem]">
+    <footer className="pt-[4rem] pb-[2.5rem]">
       <div className="px-6 mx-auto md:max-w-3xl lg:max-w-5xl xl:max-w-7xl">
         <div>
           <p className="text-muted-foreground text-sm text-center">
