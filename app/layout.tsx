d5a2f0292444eef0bd6273a13af3c51362dd19b0
Remy349 <santoraga15@gmail.com>
2025-01-29 15:22:31 -0600

    feat: Add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Header } from "./_components/header";
 import type { Metadata } from "next";
 import { nunitoSans } from "@/lib/fonts";
+import { Footer } from "./_components/footer";
 
 export const metadata: Metadata = {
   title: "ShadcnUI Multi Form",
@@ -16,7 +17,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${nunitoSans.className} antialiased`}>
         <Header />
-        <main className="mt-16">{children}</main>
+        <main className="mt-16 min-h-[calc(100vh-4rem)]">{children}</main>
+        <Footer />
       </body>
     </html>
   );
