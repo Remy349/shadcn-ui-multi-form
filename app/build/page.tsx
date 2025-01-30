8ff7517a937db9c93474f22981e8748bf1655b4d
Remy349 <santoraga15@gmail.com>
2025-01-30 00:52:10 -0600

    feat: Add form builder component

--- app/build/page.tsx
+++ app/build/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { FormBuilder } from "./_components/form-builder";
 
 export const metadata: Metadata = {
   title: "Build",
@@ -11,11 +12,12 @@ export default function Page() {
         <h1 className="text-center font-bold text-2xl mb-2 md:text-3xl">
           Craft Your Perfect Multi-Step Form Effortlessly
         </h1>
-        <p className="text-center text-sm text-muted-foreground">
+        <p className="text-center text-sm text-muted-foreground mb-12">
           The necessary components required from ShadcnUI are the Card, Button,
           Form, Label and Input components, you will also need to have React
           Hook Form installed.
         </p>
+        <FormBuilder />
       </div>
     </section>
   );
