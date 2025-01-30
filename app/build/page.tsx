6036c3d10a5aa4f83b02e7b1a83336b1db073cc2
Remy349 <santoraga15@gmail.com>
2025-01-29 19:40:08 -0600

    feat: Add build page

--- app/build/page.tsx
+++ app/build/page.tsx
@@ -6,9 +6,16 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-    <section>
-      <div>
-        <h1>Builder Page</h1>
+    <section className="pt-[4rem] pb-[2.5rem]">
+      <div className="px-6 mx-auto md:max-w-3xl lg:max-w-5xl xl:max-w-7xl">
+        <h1 className="text-center font-bold text-2xl mb-2 md:text-3xl">
+          Craft Your Perfect Multi-Step Form Effortlessly
+        </h1>
+        <p className="text-center text-sm text-muted-foreground">
+          The necessary components required from ShadcnUI are the Card, Button,
+          Form, Label and Input components, you will also need to have React
+          Hook Form installed.
+        </p>
       </div>
     </section>
   );
