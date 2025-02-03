335e121920136a65519cd83a44629e88c539b8ef
Remy349 <santoraga15@gmail.com>
2025-02-02 23:18:20 -0600

    feat: Add theme provider component and use it on layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { nunitoSans } from "@/lib/fonts";
 import { Footer } from "./_components/footer";
 import { Analytics } from "@vercel/analytics/next";
+import { ThemeProvider } from "@/providers/theme-provider";
 
 export const metadata: Metadata = {
   title: {
@@ -19,11 +20,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${nunitoSans.className} antialiased`}>
-        <Header />
-        <main className="mt-16 min-h-[calc(100vh-4rem)]">{children}</main>
-        <Footer />
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          disableTransitionOnChange
+        >
+          <div>
+            <Header />
+            <main className="mt-16 min-h-[calc(100vh-4rem)]">{children}</main>
+            <Footer />
+          </div>
+        </ThemeProvider>
         <Analytics />
       </body>
     </html>
