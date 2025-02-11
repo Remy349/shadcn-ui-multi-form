3be2a4d725a034e736e8baa8e0b398213142c4a9
Remy349 <santoraga15@gmail.com>
2025-02-10 23:08:26 -0600

    refactor: Remove update input type action

--- app/build/_components/input-field.tsx
+++ app/build/_components/input-field.tsx
@@ -2,14 +2,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useFormBuilderStore } from "@/stores/form-builder-store";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import { TInput, TInputType } from "@/types/types";
+import { TInput } from "@/types/types";
 
 interface IProps {
   formId: string;
@@ -18,8 +11,7 @@ interface IProps {
 }
 
 export const InputField = ({ formId, input, inputs }: IProps) => {
-  const { updateInputLabel, updateInputType, removeInput } =
-    useFormBuilderStore();
+  const { updateInputLabel, removeInput } = useFormBuilderStore();
 
   return (
     <div className="grid gap-y-4 border rounded-md p-4">
@@ -31,24 +23,6 @@ export const InputField = ({ formId, input, inputs }: IProps) => {
           onChange={(e) => updateInputLabel(formId, input.id, e.target.value)}
           autoComplete="off"
         />
-      </div>
-      <div className="space-x-2 flex items-center">
-        <Label htmlFor={`type-${input.id}`}>Type:</Label>
-        <Select
-          value={input.type}
-          onValueChange={(value) =>
-            updateInputType(formId, input.id, value as TInputType)
-          }
-        >
-          <SelectTrigger id={`type-${input.id}`}>
-            <SelectValue placeholder="Select input type" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="text">Text</SelectItem>
-            <SelectItem value="email">Email</SelectItem>
-            <SelectItem value="password">Password</SelectItem>
-          </SelectContent>
-        </Select>
       </div>
       {inputs.length > 1 && (
         <div>
