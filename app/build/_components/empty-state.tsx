b04abad5b09a61923bdd8a61904b9e70c1b6387b
Remy349 <santoraga15@gmail.com>
2025-02-13 13:11:14 -0600

    modify: Better indent for component

--- app/build/_components/empty-state.tsx
+++ app/build/_components/empty-state.tsx
@@ -1,10 +1,12 @@
 export const EmptyState = () => {
   return (
-    <div className="border border-dashed rounded-md w-full h-[8rem] flex flex-col items-center justify-center">
-      <h3 className="text-base font-semibold">No Inputs Added Yet!</h3>
-      <p className="text-xs text-muted-foreground">
-        Start building your form by adding input fields.
-      </p>
+    <div className="border border-dashed rounded-md">
+      <div className="flex flex-col items-center justify-center h-[8rem]">
+        <h3 className="text-base font-semibold">No Inputs Added Yet!</h3>
+        <p className="text-xs text-muted-foreground">
+          Start building your form by adding input fields.
+        </p>
+      </div>
     </div>
   );
 };
