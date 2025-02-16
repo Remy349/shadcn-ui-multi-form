2dfd7308451cc31e6115aeea1ad189d999c17212
Remy349 <santoraga15@gmail.com>
2025-02-15 23:36:14 -0600

    feat: New link added to the nav

--- app/_components/header/links.ts
+++ app/_components/header/links.ts
@@ -1,5 +1,6 @@
 export const links: { name: string; href: string }[] = [
   { name: "Home", href: "/" },
+  { name: "Components", href: "/components" },
   { name: "Build", href: "/build" },
   { name: "Releases", href: "/releases" },
 ];
