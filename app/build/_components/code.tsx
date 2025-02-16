7cba66b3ab2d056f3fb3584fe0f9c20b5c7b20cf
Remy349 <santoraga15@gmail.com>
2025-02-15 23:37:01 -0600

    modify: Modify styles and tags indent

--- app/build/_components/code.tsx
+++ app/build/_components/code.tsx
@@ -23,33 +23,34 @@ export const Code = ({ code }: IProps) => {
   };
 
   return (
-    <div className="h-full flex flex-col relative">
+    <div className="h-full flex relative">
       <Button
-        className="font-medium absolute top-4 right-8 z-40 size-8"
+        className="absolute top-4 right-8 z-40 size-8"
         size="icon"
         variant="outline"
         onClick={handleCopyToClipboard}
       >
         {isCopied ? <Check className="size-4" /> : <File className="size-4" />}
       </Button>
-      <div className="relative flex-grow overflow-auto">
-        <Highlight theme={themes.nightOwl} code={code} language="tsx">
-          {({ className, style, tokens, getLineProps, getTokenProps }) => (
-            <pre className={`${className} p-4 rounded-md`} style={style}>
-              {tokens.map((line, i) => (
-                <div key={i} {...getLineProps({ line, key: i })}>
-                  <span className="inline-block w-8 text-right mr-4 text-gray-500 select-none">
-                    {i + 1}
-                  </span>
-                  {line.map((token, key) => (
-                    <span key={key} {...getTokenProps({ token, key })} />
-                  ))}
-                </div>
-              ))}
-            </pre>
-          )}
-        </Highlight>
-      </div>
+      <Highlight theme={themes.nightOwl} code={code} language="tsx">
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre
+            className={`${className} p-4 w-full overflow-auto rounded-md`}
+            style={style}
+          >
+            {tokens.map((line, i) => (
+              <div key={i} {...getLineProps({ line, key: i })}>
+                <span className="inline-block w-8 text-right mr-4 text-gray-500 select-none">
+                  {i + 1}
+                </span>
+                {line.map((token, key) => (
+                  <span key={key} {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            ))}
+          </pre>
+        )}
+      </Highlight>
     </div>
   );
 };
