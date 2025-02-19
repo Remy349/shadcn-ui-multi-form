29f2e76468c14c6ddf27f2e9fd3aced35a4c26a8
Remy349 <santoraga15@gmail.com>
2025-02-19 11:58:31 -0600

    remove: Remove releases link from nav

--- app/_components/header/links.ts
+++ app/_components/header/links.ts
@@ -2,5 +2,4 @@ export const links: { name: string; href: string }[] = [
   { name: "Home", href: "/" },
   { name: "Components", href: "/components" },
   { name: "Build", href: "/build" },
-  { name: "Releases", href: "/releases" },
 ];
