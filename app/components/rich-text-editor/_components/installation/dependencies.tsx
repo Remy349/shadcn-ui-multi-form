8c867ed835f3080a058107178c1f626663b506b9
Remy349 <santoraga15@gmail.com>
2025-03-17 22:06:54 -0600

    feat: Add rich text editor docs page and components

---
+++ app/components/rich-text-editor/_components/installation/dependencies.tsx
@@ -0,0 +1,16 @@
+import { Code } from "@/app/components/_components/code";
+
+export const Dependencies = () => {
+  const code = `// Packages required to use Tiptap's functionalities
+npm install @tiptap/react @tiptap/pm @tiptap/starter-kit
+// Required Shadcn UI component for toolbar functionality
+npx shadcn@latest add toggle
+// Beautiful typographic defaults for HTML you don't control
+// Read more at https://tailwindcss.com/blog/tailwindcss-typography
+npm install -D @tailwindcss/typography
+// Then use the plugin in your TailwindCSS configuration file
+// TailwindCSS v3 (plugins: [require("@tailwindcss/typography")])
+// TailwindCSS v4 (@plugin "@tailwindcss/typography")`;
+
+  return <Code code={code} />;
+};
