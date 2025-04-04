4a88f7c747dd91149ddad1617951965a808a791a
Remy349 <santoraga15@gmail.com>
2025-04-03 19:53:38 -0600

    modify: Minor styles changes

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -287,9 +287,9 @@ const SidebarTrigger = React.forwardRef<
     <Button
       ref={ref}
       data-sidebar="trigger"
-      variant="ghost"
+      variant="outline"
       size="icon"
-      className={cn("h-7 w-7", className)}
+      className={cn("size-8", className)}
       onClick={(event) => {
         onClick?.(event);
         toggleSidebar();
