a12aae6fa5675f500ba794a1e0a2892a387062c4
Remy349 <santoraga15@gmail.com>
2025-04-04 19:49:57 -0600

    modify: Move builder panel to make it client component

--- app/(builder)/build/layout.tsx
+++ app/(builder)/build/layout.tsx
@@ -1,36 +1,7 @@
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar";
-import { DashboardSidebar } from "./_components/sidebar/dashboard-sidebar";
-import { ToggleDarkMode } from "@/app/(landing)/_components/header/toggle-dark-mode";
-import { CodePreviewDialog } from "./_components/code-preview/code-preview-dialog";
-
 export default function BuilderLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <div>
-      <SidebarProvider>
-        <DashboardSidebar />
-        <SidebarInset className="bg-muted/50">
-          <header className="sticky top-0 z-50 bg-background border-b">
-            <div className="h-16 flex items-center justify-between px-6">
-              <div className="flex items-center space-x-2">
-                <SidebarTrigger />
-                <CodePreviewDialog />
-              </div>
-              <ToggleDarkMode />
-            </div>
-          </header>
-          <main className="px-6">
-            <section className="pt-[2.5rem] pb-[2.5rem]">{children}</section>
-          </main>
-        </SidebarInset>
-      </SidebarProvider>
-    </div>
-  );
+  return <div>{children}</div>;
 }
