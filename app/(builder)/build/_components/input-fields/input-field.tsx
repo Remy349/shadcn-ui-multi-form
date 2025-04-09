b31d6b87a57efb482a05dad8ae255e485ce2b2a3
Remy349 <santoraga15@gmail.com>
2025-04-08 20:59:55 -0600

    modify: Modify input field styles

--- app/(builder)/build/_components/input-fields/input-field.tsx
+++ app/(builder)/build/_components/input-fields/input-field.tsx
@@ -20,12 +20,12 @@ const inputComponents: Record<TInputType, string> = {
 
 export const InputField = ({ formId, input }: IProps) => {
   return (
-    <div className="flex items-center space-x-2">
-      <div className="flex items-center border rounded-md p-2 w-full">
+    <div className="flex items-center border rounded-md p-1 w-full">
+      <div className="flex items-center">
         <Component className="size-4 mr-2" />
         <p className="text-sm">{inputComponents[input.type]}</p>
       </div>
-      <div className="flex items-center">
+      <div className="flex items-center ml-auto">
         <EditInputDialog formId={formId} input={input} />
         <RemoveInput formId={formId} inputId={input.id} />
       </div>
