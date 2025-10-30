4e4dfc93dcde73bc54fd59865679d5c74dd38b85
Remy349 <santoraga15@gmail.com>
2025-10-29 20:44:16 -0600

    modify: Update email input element to a better ui

--- app/(builder)/build/_components/form-builder/form-elements/email-input-element.tsx
+++ app/(builder)/build/_components/form-builder/form-elements/email-input-element.tsx
@@ -1,6 +1,11 @@
 import { Field, FieldDescription, FieldLabel } from "@/components/ui/field";
-import { Input } from "@/components/ui/input";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "@/components/ui/input-group";
 import { FormElement } from "@/types/form-builder";
+import { MailIcon } from "lucide-react";
 
 interface TextInputElementProps {
   element: FormElement;
@@ -10,12 +15,17 @@ export const EmailInputElement = ({ element }: TextInputElementProps) => {
   return (
     <Field>
       <FieldLabel htmlFor={element.id}>{element.label}</FieldLabel>
-      <Input
-        id={element.id}
-        autoComplete="off"
-        placeholder={element.placeholder}
-        disabled={element.disabled}
-      />
+      <InputGroup>
+        <InputGroupInput
+          id={element.id}
+          placeholder={element.placeholder}
+          autoComplete="off"
+          disabled={element.disabled}
+        />
+        <InputGroupAddon>
+          <MailIcon />
+        </InputGroupAddon>
+      </InputGroup>
       <FieldDescription>{element.description}</FieldDescription>
     </Field>
   );
