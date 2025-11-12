8201d2d212cdb2614d9909c6c8dadc869643d66a
Remy349 <santoraga15@gmail.com>
2025-11-12 00:05:25 -0600

    refactor: Toolbar refactor - Better ui design

--- app/(builder)/build/_components/form-builder/toolbar/toolbar.tsx
+++ app/(builder)/build/_components/form-builder/toolbar/toolbar.tsx
@@ -5,7 +5,7 @@ import {
   EyeIcon,
   EyeOffIcon,
   PlusIcon,
-  Settings2Icon,
+  RotateCcwIcon,
   Trash2Icon,
 } from "lucide-react";
 import {
@@ -19,14 +19,6 @@ import {
 } from "@/components/ui/select";
 import { EditForm } from "./edit-form";
 import { Separator } from "@/components/ui/separator";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { Badge } from "@/components/ui/badge";
 import { cn } from "@/lib/utils";
 
@@ -70,7 +62,7 @@ export const Toolbar = ({
         )}
       >
         {!isPreviewMode ? (
-          <div className="flex items-center space-x-4">
+          <div className="flex items-center space-x-2">
             <Button variant="secondary" size="icon-sm" onClick={addForm}>
               <PlusIcon />
             </Button>
@@ -78,64 +70,61 @@ export const Toolbar = ({
               orientation="vertical"
               className="data-[orientation=vertical]:h-4"
             />
-            <div className="flex items-center space-x-2">
-              <Select
-                value={currentForm.title}
-                onValueChange={handleSelectForm}
-              >
-                <SelectTrigger>
-                  <SelectValue placeholder="Select a form" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectLabel>Forms</SelectLabel>
-                    {forms.map((form) => (
-                      <SelectItem key={form.id} value={form.title}>
-                        {form.title}
-                      </SelectItem>
-                    ))}
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-              <div className="flex items-center space-x-1.5">
-                <EditForm currentForm={currentForm} updateForm={updateForm} />
-                {forms.length > 1 && (
-                  <Button
-                    variant="secondary"
-                    size="icon-sm"
-                    onClick={() => deleteForm(currentForm.id)}
-                  >
-                    <Trash2Icon className="text-red-500" />
-                  </Button>
-                )}
-              </div>
+            <Select value={currentForm.title} onValueChange={handleSelectForm}>
+              <SelectTrigger>
+                <SelectValue placeholder="Select a form" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Forms</SelectLabel>
+                  {forms.map((form) => (
+                    <SelectItem key={form.id} value={form.title}>
+                      {form.title}
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+            <div className="flex items-center space-x-1.5">
+              <EditForm currentForm={currentForm} updateForm={updateForm} />
+              {forms.length > 1 && (
+                <Button
+                  variant="secondary"
+                  size="icon-sm"
+                  className="text-destructive hover:text-destructive bg-destructive/10 hover:bg-destructive/10"
+                  onClick={() => deleteForm(currentForm.id)}
+                >
+                  <Trash2Icon />
+                </Button>
+              )}
             </div>
           </div>
         ) : (
           <Badge variant="secondary">Preview Mode</Badge>
         )}
         <div className="flex items-center space-x-2">
-          {!isPreviewMode && (
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button size="sm" variant="outline">
-                  <Settings2Icon /> Actions
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                  <CodeIcon /> Export Code
-                </DropdownMenuItem>
-                <DropdownMenuItem variant="destructive" onClick={clearAll}>
-                  <Trash2Icon /> Clear All
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          )}
-          <Button size="icon-sm" variant="outline" onClick={togglePreviewMode}>
+          <Button size="icon-sm" variant="secondary">
+            <CodeIcon />
+          </Button>
+          <Button
+            size="icon-sm"
+            variant="secondary"
+            onClick={togglePreviewMode}
+          >
             {isPreviewMode ? <EyeOffIcon /> : <EyeIcon />}
+          </Button>
+          <Separator
+            orientation="vertical"
+            className="data-[orientation=vertical]:h-4"
+          />
+          <Button
+            variant="secondary"
+            size="sm"
+            className="text-destructive hover:text-destructive hover:bg-destructive/10 bg-destructive/10"
+            onClick={clearAll}
+          >
+            <RotateCcwIcon />
+            Clear
           </Button>
         </div>
       </div>
