a2414eca1546d8a8c5fc80b255ff778a223cedbc
Remy349 <santoraga15@gmail.com>
2025-11-17 19:52:31 -0600

    rename: Rename editor component

--- components/ui/editor/rich-text-editor.tsx
+++ components/ui/editor/rich-text-editor.tsx
@@ -9,7 +9,7 @@ interface EditorProps {
   onChange(text: string): void;
 }
 
-export const Editor = ({ content, onChange }: EditorProps) => {
+export const RichTextEditor = ({ content, onChange }: EditorProps) => {
   const editor = useEditor({
     extensions: [StarterKit],
     content,
@@ -17,7 +17,7 @@ export const Editor = ({ content, onChange }: EditorProps) => {
     editorProps: {
       attributes: {
         class:
-          "min-h-[200px] w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          "min-h-[150px] w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
       },
     },
     onUpdate: ({ editor }) => {
