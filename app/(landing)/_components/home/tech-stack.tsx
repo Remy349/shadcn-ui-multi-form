458967b356d2be22f6f2f6ece149344ed10187d1
Remy349 <santoraga15@gmail.com>
2025-11-29 23:24:04 -0600

    modify: Minor changes

--- app/(landing)/_components/home/tech-stack.tsx
+++ app/(landing)/_components/home/tech-stack.tsx
@@ -11,7 +11,7 @@ export const TechStack = () => {
   return (
     <section className="bg-muted border-t border-b border-dashed py-[2rem]">
       <div className="px-6 max-w-5xl mx-auto flex items-center justify-center flex-col space-y-6">
-        <h2 className="font-bold text-lg md:text-xl text-center">
+        <h2 className="font-bold text-lg md:text-xl text-center tracking-tighter">
           Developed with technologies you already trust
         </h2>
         <div className="flex items-center space-x-8 text-muted-foreground">
